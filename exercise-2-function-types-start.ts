c50babda4a37bf3162727291f8a0c6c03b9ca861
marcobonni <marco.bonni19@gmail.com>
2023-03-31 00:11:19 +0200

    fix check language2

--- exercise-2-function-types-start.ts
+++ exercise-2-function-types-start.ts
@@ -25,8 +25,9 @@ function languagesSpoken(country: string, language1: string, language2?: string)
     console.log(`The languages spoken in ${country} are:`);
 
     console.log(language1);
-
+    if (typeof language2 !== undefined) {
     console.log(language2);
+    }
 }
 
 languagesSpoken("Colombia", "Spanish", "English");
